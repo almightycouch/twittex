5952c86bb6ee02a7d23abdc9c1482b9308a267a5
Fabian Frei <fabian.frei@esf-frei.ch>
2017-05-19 16:52:29 +0200

    Added support for authentication with token from dev.twitter.com

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :twittex,
-  consumer_key: "3rJOl1ODzm9yZy63FACdg",
-  consumer_secret: "5jPoQ5kQvMJFDYRNE8bQ4rHuds4xJqhvgNJM4awaE8"
-
+  consumer_key: "xxx",
+  consumer_secret: "xxx",
+  access_token: "xxx",
+  access_token_secrect: "xxx"
